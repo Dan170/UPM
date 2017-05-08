5871e0f100e791fec86432c00358319cdc850bfe
Daniel <dannutz_mic@yahoo.com>
2017-05-08 20:07:47 +0300

    Update for add_to_build.prop
    -removed duplicates;
    -removed conflicts with phone settings(touch sensitivity, button timeout, force gpu rendering);
    -rearranged some tweaks(moved from miscelaneous to the correct category);
    -removed some debug. tweaks(not doing anything to performance);
    -changed some # Signal Tweaks values(experimental, may get better or worse signal, depending form phone to phone. Usually, build.prop already has these tweaks, may be a good idea to be removed or replaced with the original ones).

--- add_to_buildprop.sh
+++ add_to_buildprop.sh
@@ -5,39 +5,25 @@ setprop=/data/setprop
 busybox echo "" >> /system/build.prop
 busybox echo "# By sohamesen && alamo" >> /system/build.prop
 
-$setprop debug.sf.hw 1
-$setprop ro.HOME_APP_ADJ 1
 $setprop video.accelerate.hw 1
 $setprop persist.sys.use_dithering 1
 # System Tweaks
-$setprop ro.HOME_APP_ADJ 1
-$setprop ro.mot.eri.losalert.delay 1000
-$setprop ro.config.hw_fast_dormancy 1
-$setprop ro.ril.enable.amr.wideband 1
 $setprop ro.config.hw_fast_dormancy 1
 $setprop ro.config.hw_quickpoweron true
 # Miscellaneous Tweaks
-$setprop persist.sys.NV_FPSLIMIT 90
-$setprop debug.qctwa.statusbar 1
-$setprop debug.qctwa.preservebuf 1
-$setprop debug.qc.hardware true
-$setprop com.qc.hardware true
 $setprop ro.telephony.sms_segment_size 160
 $setprop persist.telephony.support.ipv6 1
 $setprop persist.telephony.support.ipv4 1
-$setprop ro.config.hw_fast_dormancy 1
 $setprop persist.sys.shutdown.mode hibernate
 $setprop ro.config.hw_power_saving true
-$setprop ro.media.enc.hprof.vid.fps 65
-$setprop ro.mot.buttonlight.timeout 1
-$setprop ro.ril.set.mtu1472 1
-$setprop touch.presure.scale 0.001
 # Signal Tweaks
-$setprop ro.ril.hsxpa 1
-$setprop ro.ril.gprsclass 10
+$setprop ro.ril.hsxpa 2
+$setprop ro.ril.gprsclass 12
 $setprop ro.ril.hsdpa.category 8
 $setprop ro.ril.hsupa.category 6
 $setprop persist.cust.tel.eons 1
+$setprop ro.ril.set.mtu1472 1
+$setprop ro.config.hw_fast_dormancy 1
 # Wireless Speed Tweaks
 $setprop net.tcp.buffersize.default 4096,87380,256960,4096,16384,256960
 $setprop net.tcp.buffersize.wifi 4096,87380,256960,4096,16384,256960
@@ -78,9 +64,6 @@ $setprop ro.media.enc.jpeg.quality 100
 $setprop ro.config.nocheckin 1
 $setprop profiler.force_disable_err_rpt 1
 $setprop profiler.force_disable_ulog 1
-# Scrolling Responsiveness
-$setprop windowsmgr.max_events_per_sec 90
-$setprop persist.cust.tel.eons 1
 # Power Saving Tweaks
 $setprop ro.ril.disable.power.collapse 1
 $setprop pm.sleep_mode 1
@@ -89,6 +72,7 @@ $setprop wifi.supplicant_scan_interval 180
 $setprop ro.telephony.call_ring.delay 0
 $setprop ro.lge.proximity.delay 25
 $setprop mot.proximity.delay 25
+$setprop ro.ril.enable.amr.wideband 1
 # DalvikVM
 $setprop dalvik.vm.verify-bytecode false
 $setprop dalvik.vm.execution-mode int:fast
@@ -100,7 +84,6 @@ $setprop dalvik.vm.lockprof.threshold 250
 $setprop dalvik.vm.dexopt-flags m v,o y
 $setprop dalvik.vm.jmiopts forcecopy
 # Performance
-$setprop debug.composition.type gpu
 $setprop ro.max.fling_velocity 20000
 $setprop ro.min.fling_velocity 18000
 $setprop debug.performance.tuning 1
@@ -114,17 +97,9 @@ $setprop hw2d.force 1
 $setprop force_hw_ui true
 $setprop profiler.force_disable_err_rpt 1
 $setprop profiler.force_disable_ulog 1
-$setprop ro.min_pointer_dur 8
 $setprop debug.kill_allocating_task 0
-$setprop persist.sys.ui.hw 1
 $setprop ro.debuggable 1
-$setprop ro.config.disable.hw_accel false
 $setprop ro.kernel.android.checkjni 0
-$setprop persist.sys.purgeable_assets 1
-$setprop persist.sys.use_dithering 1
-$setprop ro.secure 0
-$setprop ro.min_pointer_dur 1
-# 16bit Transparency
-$setprop persist.sys.use_16bpp_alpha 1
+
 #  
  
